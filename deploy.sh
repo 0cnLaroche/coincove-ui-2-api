dc74a39147465e8f3dd9e8ff3017a76c093b63aa
Samuel Laroche <samuellaroche@live.ca>
2020-08-05 20:27:21 -0400

    updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-API_REPOSITORY=
-WEBAPP_REPOSITORY=https://github.com/0cnLaroche/coincove-ui-2.git
+API_APP_NAME=coincove-ui-2-api
+WEB_APP_NAME=coincove-ui-2
+API_APP_REPOSITORY=https://github.com/0cnLaroche/coincove-ui-2-api.git
+WEB_APP_REPOSITORY=https://github.com/0cnLaroche/coincove-ui-2.git
 
 installNode() 
 {
@@ -8,10 +10,44 @@ installNode()
     echo "Installing Node, Npm and Npx"
     curl -sL https://deb.nodesource.com/setup_$VERSION | sudo -E bash -
     sudo apt-get install nodejs
-    echo "Testing installations"
-    if ! command -v git &> /dev/null
-    then
+    sudo install -g forever
+    # echo "Testing installations"
+    # Do some testing here for node npm npx
+}
+
+installMongoDb()
+{
+    echo "Installing mongoDb"
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+    sudo apt-get update
+    echo "MongoDB installation completed. Lunching mongo process"
+    sudo systemctl start mongod
+    sudo systemctl enable mongod
+    sudo systemctl status mongod
+
+}
+
+updateApiApp()
+{
+    if [ ! -d "/server/$API_APP_NAME"]; then
+            echo "Server app not found, cloning into server directory"
+        mkdir server
+        (cd server && git clone $API_APP_REPOSITORY)
     fi
+    echo "Updating server app source code"
+    (cd server/$API_APP_REPOSITORY && git pull && npm run-script build)
+}
+
+updateWebApp()
+{
+    if [ ! -d "/webapp/$WEB_APP_NAME"]; then
+        echo "Web app not found, cloning into webapp directory"
+        mkdir webapp
+        (cd webapp && git clone $WEB_APP_REPOSITORY)
+    fi
+    echo "Updating web app source code"
+    (cd server/$WEB_APP_REPOSITORY && git pull && npm install)
 }
 
 echo "********** DEPLOYING *************"
@@ -30,3 +66,15 @@ then
 else 
     echo "node found"
 fi
+if ! command -v mongo &> /dev/null
+then
+    echo "mongo could not be found. Installing mongoDb ..."
+    installMongoDb
+else 
+    echo "mongoDb found"
+fi
+echo "Dependencies : OK"
+
+#updateApiApp
+#updateWebApp
+
