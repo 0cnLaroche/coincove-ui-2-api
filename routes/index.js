a29185f2131ed3975d8d203c6f45210cf2248a41
Samuel Laroche <samuellaroche@live.ca>
2020-08-05 21:45:39 -0400

    bug fix in index route
    path was not imported?

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var dotenv = require('dotenv');
-var router = express.Router();
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const router = express.Router();
 dotenv.config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(process.env.WEBAPP_DISTRIBUTION, 'build', index.html))
+  res.sendFile(path.join(process.env.WEBAPP_DISTRIBUTION, 'build', 'index.html'))
 });
 
 module.exports = router;
