99afb7f226d2f99239a7f7f919a1a408487936dc
Samuel Laroche <samuellaroche@live.ca>
2020-08-22 17:11:53 -0400

    Added patch service for orders

--- routes/orders.js
+++ routes/orders.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { Order, validateOrder, status } = require('../model/order');
 const { ValidationError, OrderError } = require('../error');
 const orderService = require('../services/orderService');
+const paypalService = require('../services/paypalService');
 const { authenticateToken } = require('../jwt');
 const logger = require('../logger');
+const { auth } = require('googleapis/build/src/apis/abusiveexperiencereport');
 const router = express.Router();
 
 /** POST new order. Validates order before accepting it */
@@ -42,8 +44,35 @@ router.post('/', async (req, res) => {
         logger.debug('Order %s saved!', order._id);
         return res.status(httpStatus ? httpStatus : 201).send(order);
     })
+});
 
-})
+router.patch('/:id', authenticateToken, (req, res) => {
+    if (!req.user.isAdmin) {
+        let msg = "User need admin access to get order details";
+        logger.debug(msg);
+        return res.status(401).json({error: msg});
+    }
+    if (req.body._id && req.body._id != req.params.id) {
+        return res.status(400).json({error: "ID in body doesn't match url"})
+    }
+    delete req.body._id;
+    Order.findOneAndUpdate({_id: req.params.id}, req.body, 
+        {returnOriginal: false, useFindAndModify: false}, (err, result) => {
+        if (err) {
+            logger.error("Could not update order %s", req.params.id);
+            logger.error(err.message);
+            return res.status(500).json({error: `Could not update order ${req.params.id}`});
+        }
+        if (result === null) {
+            return res.status(404).json({error: "No order found for patching"});
+        }
+        if (req.body.status && req.body.status === status.SHIPPED && req.body.trackingId) {
+            //TODO paypalService.postTrackingId()
+            logger.warn("Shipping information should be updated to paypal transaction for order %s", req.params.id)
+        }
+        return res.status(200).json(result);
+    });
+});
 
 /**
  * GET order by id
@@ -52,12 +81,13 @@ router.get('/:id', authenticateToken, (req, res) => {
     if (!req.user.isAdmin) {
         let msg = "User need admin access to get order details";
         logger.debug(msg);
-        return res.status(401).send(msg);
+        return res.status(401).json({error: msg});
     }
     Order.findById(req.params.id, (err, order) => {
         if(err) {
-            logger.debug("Did not find order for id %s", req.params.id);
-            return res.sendStatus(404);
+            let msg = `Did not find order for id ${req.params.id}`;
+            logger.debug(msg);
+            return res.status(404).json({error: msg});
         }
         logger.debug("Found order %s", order._id);
         return res.send(order);
@@ -71,7 +101,7 @@ router.get('/', authenticateToken, (req, res) => {
     if (!req.user.isAdmin) {
         let msg = "User need admin access to get order list";
         logger.debug(msg);
-        return res.status(401).send(msg);
+        return res.status(401).json({error: msg});
     }
     Order.find({}).exec()
     .then( data => {
@@ -79,7 +109,7 @@ router.get('/', authenticateToken, (req, res) => {
     })
     .catch( error => {
         logger.error(error.message);
-        return res.status(500).send("Could not query orders");
+        return res.status(500).json({error: "Could not query orders"});
     })
 })
 
