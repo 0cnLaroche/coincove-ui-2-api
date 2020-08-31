a547d828e47f9865e931298671837fc474dc3aa4
Samuel Laroche <samuellaroche@live.ca>
2020-08-31 13:09:22 -0400

    return 401 if user is not found

--- routes/login.js
+++ routes/login.js
@@ -7,20 +7,27 @@ var router = express.Router();
 var mockedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjb2luY292ZS11aS0yIiwibmFtZSI6IlNhbXVlbCBMYXJvY2hlIiwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJzYW11ZWxsYXJvY2hlQGxpdmUuY2EiLCJpYXQiOjE1MTYyMzkwMjJ9.hlUVKoHw1dFJq0keEhPeWQ-dkXFYf2n4FcK4e2Y6qEg";
 
 /* POST login with email/password */
-router.post("/", async (req,res) => {
+router.post("/", async (req, res) => {
     let { email, password } = req.body;
     let user = await User.findOne({email: email}).exec();
+    if (!user) {
+        return res.status(401).send();
+    }
     let hash = user.password;
     bcrypt.compare(password, hash)
         .catch(e => {
-            logger.error(password + " : " + hash)
-            return res.status(401).send("Wrong email or password");
+            logger.error("Error while comparing password and hash: { " 
+                + password + " : " + hash + "}")
+            return res.status(401).send();
         })
         .then(isSame => {
             if (isSame) {
                 const token = generateAccessToken({_id: user._id, isAdmin: user.isAdmin});
                 logger.debug('%O', {_id: user._id, token: token, isAdmin: user.isAdmin})
                 res.json({_id: user._id, token: token, isAdmin: user.isAdmin});
+            } else {
+                logger.debug("Incorrect password");
+                res.sendStatus(401);
             }
         } )
 })
